2a9de956133990a99531a7dc12602634377df1d8
Tosin <adeolamimilabake@gmail.com>
2026-05-18 17:08:22 +0100

    Number guessing app javascript

--- dayOne/JavaScript/NumberGuessingApp.js
+++ dayOne/JavaScript/NumberGuessingApp.js
@@ -1,57 +1,54 @@
-import Random;
 
 const prompt = require("prompt-sync")();
 
-        let count = 0;
-        while (true){
-        
-            let randomNumber = Math.floor(Math.Random() * 100 + 1) 
-            let userNumber = Number(prompt("Enter a number: "));
-    
-            if(userNumber < 1 || userNumber > 100)
-                console.log("only numbers between 1 and 100");
-            else
-                count += 1;
-        
-            while(count < 5){
-                
-                if (userNumber > randomNumber)
-                    console.log("Answer is lower");
-                    
-                if (userNumber < randomNumber)
-                    console.log("Answer is higher");   
+let count = 0;
+while (true){
 
-                userNumber = Number(prompt("Enter a number: "));
-                
-                if(userNumber < 1 || userNumber > 100)
-                    console.log("only numbers between 1 and 100");
-                else
-                    count += 1;
-            }
-            
-                   
-                
-        if (userNumber == randomNumber)
-            console.log("Correct"); 
-        
-        if (count == 1)
-            console.log("Rating is Legendary");   
-        else if (count == 2)
-            console.log("Rating is Excellent");
-        else if (count == 3 || count == 4)
-            console.log("Rating is Good");
-        else if (count == 5)
-            console.log("Rating is Close!") ;
-            
-        if (userNumber != randomNumber)
-            console.log("Better luck") ; 
-        
-        console.log(`Correct number is ${randomNumber}, you made ${count} attempts`);
-        break;
+    let randomNumber = Math.floor(Math.random() * 100 + 1) 
+    let userNumber = Number(prompt("Enter a number: "));
 
-        }      
+    if(userNumber < 1 || userNumber > 100)
+        console.log("only numbers between 1 and 100");
+    else
+        count += 1;
+
+    while(count < 5){
         
+        if (userNumber > randomNumber)
+            console.log("Answer is lower");
+            
+        if (userNumber < randomNumber)
+            console.log("Answer is higher");   
+
+        userNumber = Number(prompt("Enter a number: "));
+        
+        if(userNumber < 1 || userNumber > 100)
+            console.log("only numbers between 1 and 100");
+        else
+            count += 1;
     }
+    
+           
+        
+if (userNumber == randomNumber)
+    console.log("Correct"); 
 
-}
+if (count == 1)
+    console.log("Rating is Legendary");   
+else if (count == 2)
+    console.log("Rating is Excellent");
+else if (count == 3 || count == 4)
+    console.log("Rating is Good");
+else if (count == 5)
+    console.log("Rating is Close!") ;
+    
+if (userNumber != randomNumber)
+    console.log("Better luck") ; 
+
+console.log(`Correct number is ${randomNumber}, you made ${count} attempts`);
+    break;
+
+}      
+
+  
     
